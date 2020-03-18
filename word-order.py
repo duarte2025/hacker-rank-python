ed9b30ec6e4e91fa5884c9075445ebe0d54feab2
William Mateus Duarte <william.2025@hotmail.com>
2020-03-18 12:17:19 -0500

    SOLUTION to Word Order!

---
+++ word-order.py
@@ -0,0 +1,7 @@
+words = {}
+n = int(input())
+for _ in range(n):
+    key = input()
+    words[key] = words.get(key, 0) + 1
+print (len(words))
+print (*[str(words[key]) for key in words], sep = " ")
